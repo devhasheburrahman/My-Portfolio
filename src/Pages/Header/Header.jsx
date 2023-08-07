35ef92b9fd8a45525b0f28b05649cc81353d1b52
HASHEBUR RAHMAN <you@example.com>
2023-08-07 07:51:28 +0600

    :tada:modefy

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -5,9 +5,9 @@ import { FaArrowRight } from 'react-icons/fa'
 
 const Header = () => {
     return (
-        <div className='flex rounded-lg mx-3 shadow-lg  justify-center items-center pl-[60px]' style={{ backgroundImage: `url('/header_bg.png')` }}>
-            <div >
-                <div className='text-black  mb-8 pt-24 font-bold font-[Inter] text-5xl leading-[60px]'>  I’m Hashebur Rahman <br />
+        <div className='flex rounded-lg mx-3 shadow-lg  justify-center items-center pl-[30px]' style={{ backgroundImage: `url('/header_bg.png')` }}>
+            <div className='w-[500px]'>
+                <div className='text-black  mb-8 pt-24 font-bold font-[Inter] text-4xl leading-[60px]'>  I’m Hashebur Rahman <br />
                     <span className='text-[#FFB400] text-[Inter]'> Front-end</span> Developer</div>
                 <p className='font-[Inter] mb-8 text-base leading-6 capitalize text-[#767676]'>Hi, I am Hashebur a professional MERN ( MongoDB, Express, React and Node Js) Stack developer. I have 2 years of experience on this stack in a Local IT Company. I am available now on Fiverr for showing my expertise by developing your brand website .</p>
 
@@ -17,8 +17,9 @@ const Header = () => {
             </div>
 
 
-          <div>
-          <img src={'/banner1.png'} alt="" />
+          <div className=''>
+          {/* <img className='w-full' src={'/banner1.png'} alt="" /> */}
+          <img src={'/uses_client.png'} alt="" />
           </div>
 
 
